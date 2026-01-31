d9a30a9e3e2869ea13a7f1a2ec290b28aee801c5
Yvonne Wu <yw4142@log-3.hpc.nyu.edu>
2026-01-31 00:21:04 -0500

    render after env update

--- diffuser/utils/rendering.py
+++ diffuser/utils/rendering.py
@@ -364,9 +364,9 @@ class PushTRenderer:
         
         # get image given first and last observation
         self.env.reset()
-        self.env._set_state(observations[0])
+        self.env.prepare(0, observations[0])
         self.env.set_task_goal(observations[-1][2:])
-        frame = self.env._render_frame("rgb_array")
+        frame = self.env.render()
 
         H, W = frame.shape[:2]
 
