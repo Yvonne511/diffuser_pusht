117c4c70fa97a0bba5444a3137df447a768c5da1
Yvonne Wu <yw4142@torch-login-5.hpc-infra.svc.cluster.local>
2026-03-23 16:07:57 -0400

    correct render bug

--- diffuser/utils/rendering.py
+++ diffuser/utils/rendering.py
@@ -365,7 +365,7 @@ class PushTRenderer:
         # get image given first and last observation
         self.env.reset()
         self.env.prepare(0, observations[0])
-        self.env.set_task_goal(observations[-1][2:])
+        self.env.set_task_goal(observations[-1])
         frame = self.env.render()
 
         H, W = frame.shape[:2]
